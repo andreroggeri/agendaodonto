55d2995e63fed245e1089f7010c3538a61eb5320
André Roggeri Campos <a.roggeri.c@gmail.com>
2022-10-12 16:47:11 -0300

    feat: Full refactor to the new library

--- chatbot/src/bot.ts
+++ chatbot/src/bot.ts
@@ -1,128 +1,91 @@
-import { writeFileSync } from 'fs';
-import { Client, ClientOptions, Message } from 'whatsapp-web.js';
+import slugify from 'slugify';
 import * as flows from './buttons';
 import { ClientContext } from './context';
+import { Messenger } from './messaging/messenger';
 import { redis } from './redis';
 import { settings } from './settings';
 
 async function saveContext(messageAuthor: string, context: ClientContext) {
-    const content = JSON.stringify(context)
-    await redis.set(messageAuthor, content)
+  const content = JSON.stringify(context);
+  await redis.set(messageAuthor, content);
 }
 
 async function loadContext(messageAuthor: string): Promise<ClientContext> {
-    const data = await redis.get(messageAuthor)
-    if (data) {
-        return ClientContext.fromObject(JSON.parse(data))
-    }
+  const data = await redis.get(messageAuthor);
+  if (data) {
+    return ClientContext.fromObject(JSON.parse(data));
+  }
 
-    return new ClientContext(flows.initialFlow);
+  return new ClientContext(flows.initialFlow);
 }
 
-async function handleMessage(client: Client, message: Message) {
-    const context = await loadContext(message.from);
-    console.log(context);
-    if (message.body == '!ping') {
-        message.reply('pong!')
-    }
+async function handleMessage(client: Messenger, from: string, content: string) {
+  const context = await loadContext(from);
+  console.log(context);
+  if (content.includes('!ping')) {
+    console.log('replying');
+    client.sendMessage(from, 'pong');
+    client.sendButtons(from, flows.initialFlow.message, flows.initialFlow.buttons);
+  }
 
-    if (context.isLastInteractionTooOld) {
-        console.log('Last interaction was too old, restarting from the beginning')
-        flows.runFlow(flows.initialFlow, client, message)
-        context.setCurrentFlow(flows.initialFlow)
-        saveContext(message.from, context)
-        return
-    }
-
-    if (message.type.toString() === 'buttons_response') {
-        console.log('Response from a button')
-        const currentFlow = context.getCurrentFlow();
-
-        const selectedButton = currentFlow.buttons.find(b => b.text === message.body);
-
-        if (!selectedButton) {
-            client.sendMessage(message.from, 'Resposta inválida', { sendSeen: false });
-            return;
-        }
-
-        const nextFlow = selectedButton.nextFlow;
-
-        flows.runFlow(nextFlow, client, message)
-
-        context.setCurrentFlow(nextFlow);
-
-        saveContext(message.from, context);
-    }
+  if (context.isLastInteractionTooOld) {
+    console.log('Last interaction was too old, restarting from the beginning');
+    flows.runFlow(flows.initialFlow, client, from);
+    context.setCurrentFlow(flows.initialFlow);
+    saveContext(from, context);
+    return;
+  }
 }
 
-const puppeteerOptions = {
-    headless: false,
-};
-const browserlessOptions = {
-    browserWSEndpoint: settings.BROWSERLESS_URL
+async function handleButtonResponse(client: Messenger, from: string, selectedButtonId: string) {
+  const context = await loadContext(from);
+  const currentFlow = context.getCurrentFlow();
+
+  const selectedButton = currentFlow.buttons.find((b) => slugify(b.text, { lower: true }) === selectedButtonId);
+
+  if (!selectedButton) {
+    client.sendMessage(from, 'Resposta inválida');
+    return;
+  }
+
+  const nextFlow = selectedButton.nextFlow;
+
+  flows.runFlow(nextFlow, client, from);
+
+  context.setCurrentFlow(nextFlow);
+
+  saveContext(from, context);
 }
-const options: ClientOptions = {
-    puppeteer: settings.DEV_MODE ? puppeteerOptions : browserlessOptions as any,
-    session: settings.SESSION_DATA,
-    restartOnAuthFail: true,
-    takeoverOnConflict: true,
-    authTimeoutMs: 120 * 1000
-}
-const whatsapp = new Client(options);
-
-whatsapp.on('authenticated', async (credentials) => {
-    console.log('Authenticated')
-    writeFileSync('./session.json', JSON.stringify(credentials))
-});
-
-whatsapp.on('auth_failure', (err) => {
-    console.error('Authentication failure', err)
-    process.exit(1)
-})
-
-whatsapp.on('disconnected', (err) => {
-    console.error('Disconnected', err)
-    process.exit(1)
-})
-
-whatsapp.on('ready', () => {
-    console.log('ready')
-
-    setInterval(async () => {
-        const state = await whatsapp.getState()
-        console.log('State: ', state)
-        await whatsapp.getChats();
-    }, 15000);
-})
-
-whatsapp.on('change_state', (state) => {
-    console.log('Change state', state)
-})
-whatsapp.on('qr', () => {
-    console.log('qrcode')
-})
-
-whatsapp.on('message', message => {
-    console.log('New message received!!')
-    if (settings.TEST_MODE && message.body !== '!ping') {
-        console.info('TEST_MODE enabled, Ignoring message !')
-        return;
-    }
-
-    redis.lpush('messages', JSON.stringify(message))
-    console.log(JSON.stringify(message, null, 2))
-    if (message.from.includes('@g.us') || message.from.includes('@broadcast') || message.type.toString() === 'e2e_notification') {
-        console.log('Ignoring message');
-        return;
-    }
-
-    handleMessage(whatsapp, message);
-});
-
 
 (async () => {
-    console.log('Settings => ', settings)
-    await whatsapp.initialize();
-    console.log(await whatsapp.getWWebVersion())
-    console.log('Chatbot is running :)');
-})()
+  console.log('Settings => ', settings);
+
+  const messenger = new Messenger(redis);
+  await messenger.init();
+
+  messenger.onMessage(({ from, content }) => {
+    console.log('message received', { from, content, includes: !from.includes('0437') });
+    if (settings.TEST_MODE && content !== '!ping' && !from.includes('0437')) {
+      console.info('TEST_MODE enabled, Ignoring message !');
+      return;
+    }
+
+    redis.lpush('messages', JSON.stringify({ from, content }));
+
+    handleMessage(messenger, from, content);
+  });
+
+  messenger.onButtonResponse(({ from, selectedButtonId }) => {
+    console.log('button response received', { from, selectedButtonId });
+    if (settings.TEST_MODE && !from.includes('0437')) {
+      console.info('TEST_MODE enabled, Ignoring message !');
+      return;
+    }
+
+    redis.lpush('button_responses', JSON.stringify({ from, content: selectedButtonId }));
+
+    handleButtonResponse(messenger, from, selectedButtonId);
+  });
+
+  console.log('Chatbot is running :)');
+})();
