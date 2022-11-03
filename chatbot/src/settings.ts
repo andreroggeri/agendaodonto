7a24c68cee057b73f0947ddac073098fe960312d
André Roggeri Campos <a.roggeri.c@gmail.com>
2022-11-03 20:59:44 -0300

    fix: Allow the authentication folder to be configurable

--- chatbot/src/settings.ts
+++ chatbot/src/settings.ts
@@ -2,4 +2,5 @@ export const settings = {
   REDIS_URL: process.env.REDIS_URL || 'redis://localhost:6379',
   TEST_MODE: JSON.parse((process.env.TEST_MODE || 'false').toLowerCase()) as boolean,
   PORT: process.env.PORT || 3000,
+  AUTHENTICATION_PATH: process.env.AUTHENTICATION_PATH || './baileys_auth_info',
 };
