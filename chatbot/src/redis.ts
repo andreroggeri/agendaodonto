b9c7af3dea681fccbbdbdc58751f9fac724c188c
André Roggeri Campos <a.roggeri.c@gmail.com>
2022-11-03 13:28:24 -0300

    fix: Accept redis connect url

--- chatbot/src/redis.ts
+++ chatbot/src/redis.ts
@@ -1,10 +1,4 @@
-import Redis = require("ioredis");
-import { settings } from "./settings";
+import Redis = require('ioredis');
+import { settings } from './settings';
 
-
-const [REDIS_HOST, REDIS_PORT] = settings.REDIS_URL.split(':')
-
-export const redis = new Redis({
-    host: REDIS_HOST,
-    port: parseInt(REDIS_PORT),
-});
+export const redis = new Redis(settings.REDIS_URL);
--- chatbot/src/settings.ts
+++ chatbot/src/settings.ts
@@ -1,5 +1,5 @@
 export const settings = {
-  REDIS_URL: process.env.REDIS_URL || 'localhost:6379',
+  REDIS_URL: process.env.REDIS_URL || 'redis://localhost:6379',
   TEST_MODE: JSON.parse((process.env.TEST_MODE || 'false').toLowerCase()) as boolean,
   PORT: process.env.PORT || 3000,
 };
