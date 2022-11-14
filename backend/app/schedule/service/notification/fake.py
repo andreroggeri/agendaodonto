882841579fd71088c380c30f70571fdae48de4d0
André Roggeri Campos <a.roggeri.c@gmail.com>
2022-11-13 22:24:20 -0300

    feat: Update the schedule notification status

--- backend/app/schedule/service/notification/fake.py
+++ backend/app/schedule/service/notification/fake.py
@@ -1,6 +1,12 @@
+import logging
+
+from app.schedule.models import Schedule
 from app.schedule.service.notification.base import BaseNotificationService
+
+logger = logging.getLogger(__name__)
 
 
 class FakeSMS(BaseNotificationService):
-    def send_notification(self, schedule_id):
+    def send_notification(self, schedule: Schedule) -> bool:
+        logger.info(f'Fake SMS sent to {schedule.patient.phone}')
         return True
