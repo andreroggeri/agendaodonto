3772438c810f3f6a0d012f651ad264600900e6ba
André Roggeri Campos <a.roggeri.c@gmail.com>
2023-02-19 23:02:52 -0300

    feat(backend): Allow filtering treatment requests

--- backend/app/schedule/views/treatment_request.py
+++ backend/app/schedule/views/treatment_request.py
@@ -1,3 +1,5 @@
+from django_filters import CharFilter
+from django_filters.rest_framework import FilterSet
 from rest_framework import permissions
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
@@ -9,12 +11,21 @@ from app.schedule.serializers.treatment_request import TreatmentRequestSerialize
 from app.schedule.tasks import submit_basic_treatment_request
 
 
+class TreatmentRequestFilter(FilterSet):
+    status = CharFilter(field_name='status')
+
+    class Meta:
+        model = TreatmentRequest
+        fields = ['status']
+
+
 class TreatmentRequestList(ListCreateAPIView):
     """
     Lista de solicitações de tratamento
     """
     permission_classes = (permissions.IsAuthenticated | IsApiKeyValid,)
     serializer_class = TreatmentRequestSerializer
+    filter_class = TreatmentRequestFilter
 
     def get_queryset(self):
         if self.request.user and self.request.user.is_authenticated:
