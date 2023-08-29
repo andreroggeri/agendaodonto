78ff1aabf0bd5f7f36a7acb99484e429bc21fc41
André Roggeri Campos <a.roggeri.c@gmail.com>
2023-08-29 12:28:00 -0300

    fix(backend): Execute entrypoint command

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -14,4 +14,7 @@ if [ $# -eq 0 ]; then
   PORT=${PORT:-8000}
   echo "Starting gunicorn on port ${PORT}"
   exec gunicorn app.wsgi -b "0.0.0.0:$PORT" --log-file -
+else
+  echo "Running command $@"
+  exec "$@"
 fi
