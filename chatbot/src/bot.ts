651ad7066b9b3277eb476cf2af39e562013da67c
André Roggeri Campos <a.roggeri.c@gmail.com>
2024-03-30 16:50:22 -0300

    chore: Re-release

--- chatbot/src/bot.ts
+++ chatbot/src/bot.ts
@@ -125,7 +125,7 @@ async function ensureDependencies() {
     await db.$connect();
     logger.info('Database is up and running');
   } catch (err) {
-    logger.error('Failed to connect to database', err);
+    logger.error('Failed to connect to database.', err);
     process.exit(1);
   }
 }
